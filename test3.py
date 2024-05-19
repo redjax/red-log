00a87121a047d923cd8fe909c888b6eca19e08a8
redjax <none@none.com>
2024-05-19 15:22:38 -0400

    Continue development in test3.py

--- test3.py
+++ test3.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 import typing as t
 
 from queue import Queue
@@ -14,6 +15,17 @@ class BaseLoggingConfig(ABC):
     @abstractmethod
     def get_configdict(self) -> None:
         pass
+
+
+@dataclass
+class FilterConfig(BaseLoggingConfig):
+    name: str
+    func: callable
+
+    def get_filter(self) -> logging.Filter:
+        filter_obj = logging.Filter(name=self.name)
+        filter_obj.filter = self.func
+        return filter_obj
 
 
 @dataclass
@@ -36,9 +48,10 @@ class FormatterConfig(BaseLoggingConfig):
 
 @dataclass
 class BaseHandlerConfig(BaseLoggingConfig):
-    name: str
-    level: str
-    formatter: str
+    name: str = None
+    level: str = "NOTSET"
+    formatter: str = None
+    propagate: bool = True
 
     @abstractmethod
     def get_configdict(self) -> None:
@@ -47,7 +60,7 @@ class BaseHandlerConfig(BaseLoggingConfig):
 
 @dataclass
 class StreamHandlerConfig(BaseHandlerConfig):
-    stream: t.Any | None = None
+    stream: t.Any | None = "ext://sys.stdout"
 
     def get_configdict(self) -> dict[str, dict[str, str]]:
         handler_dict: dict[str, dict[str, str]] = {
@@ -55,6 +68,7 @@ class StreamHandlerConfig(BaseHandlerConfig):
                 "class": "logging.StreamHandler",
                 "level": self.level,
                 "formatter": self.formatter,
+                "propagate": self.propagate,
             }
         }
         if self.stream is not None:
@@ -64,7 +78,7 @@ class StreamHandlerConfig(BaseHandlerConfig):
 
 @dataclass
 class FileHandlerConfig(BaseHandlerConfig):
-    filename: str
+    filename: str | None = field(default="app.log")
 
     def get_configdict(self) -> dict[str, dict[str, str]]:
         handler_dict: dict[str, dict[str, str]] = {
@@ -73,6 +87,7 @@ class FileHandlerConfig(BaseHandlerConfig):
                 "level": self.level,
                 "formatter": self.formatter,
                 "filename": self.filename,
+                "propagate": self.propagate,
             }
         }
         return handler_dict
@@ -80,7 +95,7 @@ class FileHandlerConfig(BaseHandlerConfig):
 
 @dataclass
 class RotatingFileHandlerConfig(BaseHandlerConfig):
-    filename: str
+    filename: str | None = field(default="app.log")
     maxBytes: int = 0
     backupCount: int = 0
 
@@ -93,6 +108,7 @@ class RotatingFileHandlerConfig(BaseHandlerConfig):
                 "filename": self.filename,
                 "maxBytes": self.maxBytes,
                 "backupCount": self.backupCount,
+                "propagate": self.propagate,
             }
         }
         return handler_dict
@@ -100,8 +116,8 @@ class RotatingFileHandlerConfig(BaseHandlerConfig):
 
 @dataclass
 class TimedRotatingFileHandlerConfig(BaseHandlerConfig):
-    filename: str
-    when: str
+    filename: str | None = field(default="app.log")
+    when: str | None = field(default="midnight")
     interval: int = 1
     backupCount: int = 0
 
@@ -115,6 +131,7 @@ class TimedRotatingFileHandlerConfig(BaseHandlerConfig):
                 "when": self.when,
                 "interval": self.interval,
                 "backupCount": self.backupCount,
+                "propagate": self.propagate,
             }
         }
         return handler_dict
@@ -122,8 +139,8 @@ class TimedRotatingFileHandlerConfig(BaseHandlerConfig):
 
 @dataclass
 class SocketHandlerConfig(BaseHandlerConfig):
-    host: str
-    port: int
+    host: str = "localhost"
+    port: int = 0
 
     def get_configdict(self) -> dict[str, dict[str, t.Any]]:
         handler_dict: dict[str, dict[str, t.Any]] = {
@@ -133,6 +150,7 @@ class SocketHandlerConfig(BaseHandlerConfig):
                 "formatter": self.formatter,
                 "host": self.host,
                 "port": self.port,
+                "propagate": self.propagate,
             }
         }
         return handler_dict
@@ -140,10 +158,10 @@ class SocketHandlerConfig(BaseHandlerConfig):
 
 @dataclass
 class SMTPHandlerConfig(BaseHandlerConfig):
-    mailhost: t.Any
-    fromaddr: str
-    toaddrs: list
-    subject: str
+    mailhost: t.Any = None
+    fromaddr: str = "from@example.com"
+    toaddrs: list = field(default_factory=lambda: [])
+    subject: str = "SMTPHandler Log Event"
     credentials: tuple | None = None
     secure: tuple | None = None
 
@@ -157,6 +175,7 @@ class SMTPHandlerConfig(BaseHandlerConfig):
                 "fromaddr": self.fromaddr,
                 "toaddrs": self.toaddrs,
                 "subject": self.subject,
+                "propagate": self.propagate,
             }
         }
         if self.credentials:
@@ -168,7 +187,7 @@ class SMTPHandlerConfig(BaseHandlerConfig):
 
 @dataclass
 class QueueHandlerConfig(BaseHandlerConfig):
-    queue: Queue
+    queue: Queue = field(default=None)
 
     def get_configdict(self) -> dict[str, dict[str, t.Any]]:
         handler_dict: dict[str, dict[str, t.Any]] = {
@@ -177,6 +196,7 @@ class QueueHandlerConfig(BaseHandlerConfig):
                 "level": self.level,
                 "formatter": self.formatter,
                 "queue": self.queue,
+                "propagate": self.propagate,
             }
         }
         return handler_dict
@@ -200,16 +220,40 @@ class QueueListenerConfig(BaseLoggingConfig):
 
 
 @dataclass
+class LoggerConfig(BaseLoggingConfig):
+    name: str
+    level: str
+    handlers: list
+    propagate: bool = True
+
+    def get_configdict(self) -> dict:
+        logger_dict: dict[str, dict[str, t.Any]] = {
+            self.name: {
+                "level": self.level,
+                "handlers": self.handlers,
+                "propagate": self.propagate,
+            }
+        }
+        return logger_dict
+
+
+@dataclass
 class LoggingConfig:
     """Assemble a logging configdict from input handlers, formatters, and loggers."""
 
     version: int = 1
     disable_existing_loggers: bool = False
     propagate: bool = True
-    root: dict = field(default_factory=dict)
+    # root: dict = field(default_factory=dict)
     formatters: dict = field(default_factory=dict)
     handlers: dict = field(default_factory=dict)
     loggers: dict = field(default_factory=dict)
+
+    ## Root logger config overrides
+    root_level: str = "WARNING"
+    root_formatter: str = "[%(levelname)s][%(filename)s:%(lineno)d]: %(message)s"
+    root_datefmt: str = "%Y-%m-%d %H:%M:%S"
+    root_handlers: list = field(default_factory=lambda: ["root_console"])
 
     def add_formatters(self, formatter_configs: list[FormatterConfig]):
         for formatter in formatter_configs:
@@ -219,12 +263,33 @@ class LoggingConfig:
         for handler in handler_configs:
             self.handlers.update(handler.get_configdict())
 
+    def add_loggers(self, logger_configs: list[LoggerConfig]):
+        for logger in logger_configs:
+            self.loggers.update(logger.get_configdict())
+
     def get_config(self) -> dict:
+        root_formatter_config = FormatterConfig(
+            name="root_formatter", fmt=self.root_formatter, datefmt=self.root_datefmt
+        )
+        root_handler_config = StreamHandlerConfig(
+            name="root_console",
+            level="DEBUG",
+            formatter="root_formatter",
+            stream="ext://sys.stdout",
+        )
+
+        ## Define the root logger configuration
+        root_logger_config: dict[str, t.Any] = {
+            "level": self.root_level,
+            "handlers": self.root_handlers,
+            "propagate": self.propagate,
+        }
+
         return {
             "version": self.version,
             "disable_existing_loggers": self.disable_existing_loggers,
             "propagate": self.propagate,
-            "root": self.root,
+            "root": root_logger_config,
             "formatters": self.formatters,
             "handlers": self.handlers,
             "loggers": self.loggers,
@@ -301,6 +366,12 @@ if __name__ == "__main__":
         name="queue_listener", queue=queue, handlers=["file", "console"]
     )
 
+    custom_logger = LoggerConfig(
+        name="custom",
+        level="INFO",
+        handlers=[StreamHandlerConfig(name="console_custom", formatter=formatter)],
+    )
+
     logging_config = LoggingConfig()
     logging_config.add_formatters([formatter])
     logging_config.add_handlers(
@@ -314,5 +385,6 @@ if __name__ == "__main__":
             queue_handler,
         ]
     )
+    logging_config.add_loggers([custom_logger])
 
     print(f"Logging config: {logging_config.get_config()}")
